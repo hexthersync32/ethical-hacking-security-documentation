ea0e2e548a374a8b3d84bbd8ad44365649a1e29e
lucaxgomex <lg.projectdev@gmail.com>
2026-02-03 17:14:14 -0300

    new/astro-template: added section folders

--- ethical-hacking-docs/astro.config.mjs
+++ ethical-hacking-docs/astro.config.mjs
@@ -6,14 +6,40 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '@hexthersync32',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Computer Architecture',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Server', slug: 'computer-architecture/example' },
+						{ label: 'Ports', slug: 'computer-architecture/example02' },
+					],
+				},
+				{
+					label: 'Cyber Security Principles',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Cyber Security Principles', slug: 'security-basics/01' },
+						{ label: 'Enumeration', slug: 'security-basics/02' },
+						{ label: 'Ethical Hacking', slug: 'security-basics/03' },
+						{ label: 'Footprinting', slug: 'security-basics/04' },
+					],
+				},
+				{
+					label: 'Protocols',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Protocols', slug: 'protocols/01' },
+					],
+				},
+				{
+					label: 'Networking',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'OSI Model', slug: 'networking/01' },
+						{ label: 'CIDR', slug: 'networking/02' },
 					],
 				},
 				{
