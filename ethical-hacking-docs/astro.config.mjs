5af60a4a63a5d1001413847b196e729d40316f30
lucaxgomex <lg.projectdev@gmail.com>
2026-02-17 21:33:24 -0300

    new/astro-template: new labels for docs

--- ethical-hacking-docs/astro.config.mjs
+++ ethical-hacking-docs/astro.config.mjs
@@ -7,11 +7,30 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: '@hexthersync32',
+			logo: {
+				src: './src/assets/logo.svg',
+				replacesTitle: true,
+			},
 			customCss: [
 				// Relative path to your custom CSS file
 				'./src/styles/index.css',
 			],
 			social: [
+				{ 
+					icon: 'seti:python', 
+					label: 'HackerRank', 
+					href: 'https://github.com/lucaxgomex' 
+				},
+				{ 
+					icon: 'document', 
+					label: 'Medium', 
+					href: 'https://github.com/lucaxgomex' 
+				},
+				{ 
+					icon: 'linkedin', 
+					label: 'LinkedIn', 
+					href: 'https://github.com/lucaxgomex' 
+				},
 				{ 
 					icon: 'github', 
 					label: 'GitHub', 
@@ -22,10 +41,32 @@ export default defineConfig({
 				{
 					label: 'Computer Architecture',
 					items: [
-						{ label: 'Components', slug: 'computer-architecture/01' },
+						{ 
+							label: 'The Von Neuman Architecture', 
+							slug: 'computer-architecture/01' 
+						},
+						{ 
+							label: 'Components',
+							items: [
+								{ label: 'Central Processing Unity', slug: 'computer-architecture/01' },		
+								{ label: 'Motherboard', slug: 'computer-architecture/01' },		
+								{ label: 'Chipset', slug: 'computer-architecture/01' },		
+								{ label: 'RAM Memory', slug: 'computer-architecture/01' },		
+								{ label: 'Hard Disk Drive', slug: 'computer-architecture/01' },
+								{ label: 'Solid State Drive', slug: 'computer-architecture/01' },		
+								{ label: 'Font', slug: 'computer-architecture/01' },	
+							]
+						},
 						{ label: 'Machine Language', slug: 'computer-architecture/02' },
 						{ label: 'Ports', slug: 'computer-architecture/02' },
 						{ label: 'Server', slug: 'computer-architecture/03' },
+					],
+				},
+				{
+					label: 'Ethical Hacking',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'What it is?', slug: 'ethical-hacking/01' },
 					],
 				},
 				{
@@ -52,7 +93,6 @@ export default defineConfig({
 						{ label: 'OSI Model', slug: 'networking/01' },
 						{ label: 'CIDR', slug: 'networking/02' },
 						{ label: 'Domain Name', slug: 'networking/03' },
-						{ label: 'Devices', slug: 'networking/04' },
 						{ label: 'Sockets', slug: 'networking/05' },
 					],
 				},
@@ -91,9 +131,35 @@ export default defineConfig({
 				{
 					label: 'Network Protocols',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'DNS', slug: 'network-protocols/01' },
-						{ label: 'HTTP', slug: 'network-protocols/02' },
+						{ 
+							label: 'DNS', 
+							items: [
+								{ 
+									label: 'Introduction', 
+									slug: 'network-protocols/dns/01' 
+								},
+								{ 
+									label: 'DNS Zone', 
+									slug: 'network-protocols/dns/02' 
+								},
+								{ 
+									label: 'Main Attacks', 
+									slug: 'network-protocols/dns/03' 
+								},
+								{ 
+									label: 'FAQ', 
+									slug: 'network-protocols/dns/04' 
+								},
+								{ 
+									label: 'Discovering', 
+									slug: 'network-protocols/dns/05' 
+								},
+							]
+						},
+						{ 
+							label: 'HTTP', 
+							slug: 'network-protocols/02' 
+						},
 					],
 				},
 				{
@@ -103,28 +169,77 @@ export default defineConfig({
 					],
 				},
 				{
+					label: 'GNU/Linux',
+					items: [
+						{ 
+							label: 'The Von Neuman Architecture', 
+							slug: 'computer-architecture/01' 
+						},
+						{ 
+							label: 'Components',
+							items: [
+								{ label: 'Central Processing Unity', slug: 'computer-architecture/01' },		
+								{ label: 'Motherboard', slug: 'computer-architecture/01' },		
+								{ label: 'Chipset', slug: 'computer-architecture/01' },		
+								{ label: 'RAM Memory', slug: 'computer-architecture/01' },		
+								{ label: 'Hard Disk Drive', slug: 'computer-architecture/01' },
+								{ label: 'Solid State Drive', slug: 'computer-architecture/01' },		
+								{ label: 'Font', slug: 'computer-architecture/01' },	
+							]
+						},
+						{ label: 'Machine Language', slug: 'computer-architecture/02' },
+						{ label: 'Ports', slug: 'computer-architecture/02' },
+						{ label: 'Server', slug: 'computer-architecture/03' },
+					],
+				},
+				{
 					label: 'Utilities',
 					items: [
-						{ label: 'Dig', slug: 'utilities/01' },
-						{ label: 'Curl', slug: 'utilities/02' },
-						{ label: 'Nmap', slug: 'utilities/03' },
-						{ label: 'Nslookup', slug: 'utilities/04' },
-						{ label: 'Ping', slug: 'utilities/05' },
-						{ label: 'Whois', slug: 'utilities/06' },
+						{ label: 'dig', slug: 'utilities/01' },
+						{ label: 'nurl', slug: 'utilities/02' },
+						{ label: 'nmap', slug: 'utilities/03' },
+						{ label: 'nslookup', slug: 'utilities/04' },
+						{ label: 'ping', slug: 'utilities/05' },
+						{ label: 'whois', slug: 'utilities/06' },
+						{ label: 'whatweb', slug: 'utilities/07' },
 					],
 				},
 				{
 					label: 'Technical Support Tools',
 					items: [
-						{ label: 'WinDirStat', slug: 'technical-support/01' },
-						{ label: 'Rundeck', slug: 'technical-support/02' },
-						{ label: 'Octadesk', slug: 'technical-support/03' },
-						{ label: 'CPU-Z', slug: 'technical-support/04' },
-						{ label: 'OCCT', slug: 'technical-support/05' },
-						{ label: 'Cinebench', slug: 'technical-support/06' },
-						{ label: 'Hiren\'s BootCD', slug: 'technical-support/07' },
-						{ label: 'Lazersoft', slug: 'technical-support/07' },
-						{ label: 'Have I Been Pwned?', slug: 'technical-support/07' },
+						{ 
+							label: 'Third-Party Software',
+							items: [
+								{ label: 'WinDirStat', slug: 'technical-support/01' },
+								{ label: 'Octadesk', slug: 'technical-support/03' },
+								{ label: 'CPU-Z', slug: 'technical-support/04' },
+								{ label: 'OCCT', slug: 'technical-support/05' },
+								{ label: 'Cinebench', slug: 'technical-support/06' },   
+								{ label: 'Lazersoft', slug: 'technical-support/08' },
+								{ label: 'Malwarebytes', slug: 'technical-support/10' },
+							] 
+						},
+						{ 
+							label: 'Windows OS Solutions',
+							items: [
+								{ label: 'Windows Security', slug: 'technical-support/11' },
+								{ label: 'diskmgmt.msc', slug: 'technical-support/12' },             
+								{ label: 'chkdsk', slug: 'technical-support/13' },
+							] 
+						},
+						{ 
+							label: 'Browser Solutions',
+							items: [
+								{ label: 'Rundeck', slug: 'technical-support/02' },
+								{ label: 'Have I Been Pwned?', slug: 'technical-support/09' },
+							] 
+						},
+						{ 
+							label: 'Flash Boot',
+							items: [
+								{ label: 'Hiren\'s BootCD', slug: 'technical-support/07' },
+							] 
+						},
 					],
 				},
 				{
@@ -137,7 +252,13 @@ export default defineConfig({
 				{
 					label: 'Malicious Software',
 					items: [
-						{ label: 'Total Virus', slug: 'malware/01' },
+						{ label: 'Malware', slug: 'malware/01' },
+						{ 
+							label: 'Categories',
+							items: [
+								{ label: 'Ransomware', slug: 'malware/categories/02' },        
+							] 
+						},
 					],
 				},
 				{
